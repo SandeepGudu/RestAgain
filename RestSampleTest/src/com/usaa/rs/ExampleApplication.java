29beeaa0a8702d2db0c9cf88aa1d863780fff14d
sandeep <sandeep@192.168.0.101>
2017-09-08 01:39:26 +0530

    This is First Commit

---
+++ RestSampleTest/src/com/usaa/rs/ExampleApplication.java
@@ -0,0 +1,9 @@
+package com.usaa.rs;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+ 
+@ApplicationPath("/rest")
+public class ExampleApplication extends Application {
+ 
+}
---
+++ RestSampleTest/src/com/usaa/rs/HelloWorldService.java
@@ -0,0 +1,15 @@
+package com.usaa.rs;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/HelloWorld")
+public class HelloWorldService {
+
+	@GET
+	@Path("/sayHello")
+	public String sayHello() {
+		return "<h1>Hello World</h1>";
+	}
+
+}
